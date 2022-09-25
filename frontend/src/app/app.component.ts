a37bdd9859e10633b59710cb4d2274401e2c187c
Jason Kulatunga <jason@thesparktree.com>
2022-09-25 11:46:15 -0700

    limit manual bundle upload to JSON files make sure header hidden on login and signup pages.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +13,27 @@ export class AppComponent implements OnInit {
   showHeader:boolean = true;
   showFooter:boolean = true;
 
+  constructor(private router: Router) {}
+
   ngOnInit() {
 
     // navbar backdrop for mobile only
     const navbarBackdrop = document.createElement('div');
     navbarBackdrop.classList.add('az-navbar-backdrop');
     document.querySelector('body').appendChild(navbarBackdrop);
+
+
+    //determine if we should show the header
+    this.router.events.subscribe(event => this.modifyHeader(event));
+  }
+
+  modifyHeader(location) {
+    if(location.url.startsWith('/auth'))
+    {
+      this.showHeader = false;
+    } else {
+      this.showHeader = true;
+    }
   }
 }
 
