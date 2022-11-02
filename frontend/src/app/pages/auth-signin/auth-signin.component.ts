6f358cb79bdc577695271c9c23088e656f199ab4
Jason Kulatunga <jason@thesparktree.com>
2022-11-02 00:11:57 -0700

    for 3rd party idp, redirect to dashboard on login.

--- frontend/src/app/pages/auth-signin/auth-signin.component.ts
+++ frontend/src/app/pages/auth-signin/auth-signin.component.ts
@@ -37,7 +37,14 @@ export class AuthSigninComponent implements OnInit {
       let state = params.get('state') // eyJhbGciOiJSUzI1...rest_of_ID_Token
 
       this.resetUrlOnCallback()
-      this.authService.IdpCallback(idpType, state, code).then(console.log)
+      this.authService.IdpCallback(idpType, state, code)
+        .then(() => this.router.navigateByUrl('/dashboard'))
+        .catch((err)=>{
+          const toastNotification = new ToastNotification()
+          toastNotification.type = ToastType.Error
+          toastNotification.message = "an error occurred while signing in"
+          this.toastService.show(toastNotification)
+        })
     }
 
   }
