e0ea502e0d8e0ccda9ead182b468f0b6e5f2f006
Jason Kulatunga <jason@thesparktree.com>
2022-12-21 23:59:09 -0800

    adding charts to labs.

--- frontend/src/app/components/report-labs-observation/report-labs-observation.component.ts
+++ frontend/src/app/components/report-labs-observation/report-labs-observation.component.ts
@@ -3,6 +3,8 @@ import {ResourceFhir} from '../../models/fasten/resource_fhir';
 import {ChartOptions} from 'chart.js';
 // import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 // import { BaseChartDirective } from 'ng2-charts';
+import * as fhirpath from 'fhirpath';
+
 
 @Component({
   selector: 'app-report-labs-observation',
@@ -15,74 +17,127 @@ export class ReportLabsObservationComponent implements OnInit {
   @Input() observationCode: string
   @Input() observationTitle: string
 
-  // barChartData = [{
-  //   label: '# of Votes',
-  //   stack: 1,
-  //   data: [[12,20], 39, 20, 10, 55, 18],
-  // },{
-  //   label: '# of Votes',
-  //   stack: 2,
-  //   data: [12, 39, 20, 10, 55, 18],
-  // },{
-  //   label: '# of Votes',
-  //   stack: 3,
-  //   data: [12, 39, 20, 10, 55, 18],
-  // },{
-  //   label: '# of Votes',
-  //   stack: 4,
-  //   data: [12, 39, 20, 10, 55, 18],
-  // }];
+  chartHeight = 40
+
+  barChartData =[
+    // {
+    //   label: "Current",
+    //   backgroundColor: 'rgba(255, 0, 128, 1)',
+    //   data: [],
+    //   xAxisID: "x-axis-current"
+    // },
+    // {
+    //   label: "Reference",
+    //   backgroundColor: 'rgba(99,189,50,0.2)',
+    //   data: [],
+    //   xAxisID: "x-axis-ref"
+    // },
 
 
-  barChartData = [{
-    label: 'Line 1',
-    data: [12, 39, 20, 10, 55, 18],
-    backgroundColor: '#3788d8cc',
-    borderColor: '#3788d8cc',
-    fill: false
-  },  {
-    label: 'Line 2',
-    data: [82, 49, 30, 10, 25, 18],
-    backgroundColor: '#ff9800',
-    borderColor: '#ff9800',
-    fill: false
-  }]
 
-  barChartLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
+    {
+      label: "Reference",
+      data: [[55,102], [44,120]],
+      backgroundColor: "rgba(91, 71, 251,0.6)",
+      hoverBackgroundColor: "rgba(91, 71, 251,0.2)"
+    },{
+      label: "Current",
+      data: [[80,81], [130,131]],
+      borderColor: "rgba(0,0,0,1)",
+      backgroundColor: "rgba(0,0,0,1)",
+      hoverBackgroundColor: "rgba(0,0,0,1)",
+      minBarLength: 3,
+      barPercentage: 1,
+      tooltip: {
+
+      }
+      // id: "x-axis-current",
+      //important settings
+
+      //set the width of the line ( or point )
+      // pointRadius: 50,
+      // don´t show line betrween points
+      // showLine: false,
+      //change points of line chart to line style ( little bit confusin why it´s named point anyway )
+      // pointStyle: 'line',
+
+      //chart type
+      // type: "line",
+    }
+  ]
+
+  barChartLabels = [] // ["2020", "2018"] //["1","2","3","4","5","6","7","8"]
 
   barChartOptions = {
+    legend:{
+      display: false,
+    },
+    //add padding to fix tooltip cutoff
+    layout: {
+      padding: {
+        left: 0,
+        right: 4,
+        top: 0,
+        bottom: 10
+      }
+    },
     scales: {
       yAxes: [{
         stacked: true,
         ticks: {
           beginAtZero: true,
           fontSize: 10,
-          min:0,
-          max: 80
+          min: 0,
+          // max: 80
         }
       }],
       xAxes: [{
-        stacked: true,
-        // barPercentage: 0.6,
+        // stacked: true,
         ticks: {
-          beginAtZero:true,
-          fontSize: 11
+          beginAtZero: true,
+          fontSize: 10,
+          min: 0,
+          // max: 80
         }
-      }]
-    },
-    legend: {
-      display: false
-    },
-    elements: {
-      point: {
-        radius: 0
-      }
+      }],
+
     }
+    //   xAxes: [{
+    //     id: "x-axis-current",
+    //     stacked: true,
+    //     // barPercentage: 0.6,
+    //     ticks: {
+    //       beginAtZero:true,
+    //       fontSize: 11
+    //     }
+    //   },{
+    //     id: "x-axis-ref",
+    //     stacked: true,
+    //     ticks: {
+    //       beginAtZero:true,
+    //       fontSize: 11
+    //     }
+    //     // offset: true,
+    //
+    //     // display: false,
+    //     // gridLines: {
+    //     //   offsetGridLines: true
+    //     // }
+    //   }]
+    // },
+    // legend: {
+    //   display: false
+    // },
+    // elements: {
+    //   point: {
+    //     radius: 0
+    //   }
+    // }
   } as ChartOptions
 
   barChartColors = [
     {
-      backgroundColor: '#560bd0'
+      backgroundColor: 'white'
     }
   ];
 
@@ -90,6 +145,32 @@ export class ReportLabsObservationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    let currentValues = []
+
+    let referenceRanges = []
+
+    for(let observation of this.observations){
+      //get label
+      this.barChartLabels.push(fhirpath.evaluate(observation.resource_raw, "Observation.effectiveDateTime")[0])
+
+      //get current value
+      currentValues.push(fhirpath.evaluate(observation.resource_raw, "Observation.valueQuantity.value")[0])
+
+      //add low/high ref value blocks
+      referenceRanges.push([
+        fhirpath.evaluate(observation.resource_raw, "Observation.referenceRange.low.value")[0],
+        fhirpath.evaluate(observation.resource_raw, "Observation.referenceRange.high.value")[0]
+      ])
+    }
+
+    // @ts-ignore
+    this.barChartData[0].data = referenceRanges
+    this.barChartData[1].data = currentValues.map(v => [v, v])
+
+    if(currentValues.length > 1){
+      this.chartHeight = 30 * currentValues.length
+    }
   }
 
 }
