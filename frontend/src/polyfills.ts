3692fd462f830115f9eb2fd7af81ed7b5f32aa16
Jason Kulatunga <jason@thesparktree.com>
2023-02-05 18:35:14 -0800

    adding polyfill for subtle crypto, so container can be accessed by IP (#59)

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -27,7 +27,7 @@
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
  */
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
+import 'webcrypto-liner'
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
