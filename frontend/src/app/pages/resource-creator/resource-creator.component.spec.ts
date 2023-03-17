e64aaa56dd0c693ab1c2df589637ef37f420873c
Jason Kulatunga <jason@thesparktree.com>
2023-03-16 23:17:12 -0700

    fixing tests.

--- frontend/src/app/pages/resource-creator/resource-creator.component.spec.ts
+++ frontend/src/app/pages/resource-creator/resource-creator.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ResourceCreatorComponent } from './resource-creator.component';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {RouterTestingModule} from '@angular/router/testing';
+import {NgbCollapseModule, NgbDatepickerModule} from '@ng-bootstrap/ng-bootstrap';
 
 describe('ResourceCreatorComponent', () => {
   let component: ResourceCreatorComponent;
@@ -10,7 +11,7 @@ describe('ResourceCreatorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule, NgbDatepickerModule, NgbCollapseModule],
       declarations: [ ResourceCreatorComponent ]
     })
     .compileComponents();
