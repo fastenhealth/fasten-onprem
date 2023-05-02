47e9cbb4b03095e4172be179f04e21341d19bfbf
Jason Kulatunga <jason@thesparktree.com>
2023-05-01 19:39:31 -0700

    adding version information to the footer.

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {versionInfo} from '../../../environments/versions';
 
 @Component({
   selector: 'app-footer',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  appVersion: string;
 
-  constructor() { }
+  constructor() {
+    this.appVersion = versionInfo.version
+  }
 
   ngOnInit() {
   }
