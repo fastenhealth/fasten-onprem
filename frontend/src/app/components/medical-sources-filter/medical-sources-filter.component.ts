fa4e9467bc0b0805ae9643e6936a8c73aec11e0c
Jason Kulatunga <jason@thesparktree.com>
2023-05-07 16:42:47 -0700

    start working on a source fitlering service.

---
+++ frontend/src/app/components/medical-sources-filter/medical-sources-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medical-sources-filter',
+  templateUrl: './medical-sources-filter.component.html',
+  styleUrls: ['./medical-sources-filter.component.scss']
+})
+export class MedicalSourcesFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
