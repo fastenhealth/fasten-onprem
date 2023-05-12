aecfe8dd9a1488b8d58c488ae9200910f017661b
Jason Kulatunga <jason@thesparktree.com>
2023-05-12 09:12:09 -0400

    working callbacks, working search filtering/facets

--- frontend/src/app/components/medical-sources-filter/medical-sources-filter.component.spec.ts
+++ frontend/src/app/components/medical-sources-filter/medical-sources-filter.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MedicalSourcesFilterComponent } from './medical-sources-filter.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 describe('MedicalSourcesFilterComponent', () => {
   let component: MedicalSourcesFilterComponent;
@@ -8,6 +9,7 @@ describe('MedicalSourcesFilterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [FormsModule, ReactiveFormsModule],
       declarations: [ MedicalSourcesFilterComponent ]
     })
     .compileComponents();
