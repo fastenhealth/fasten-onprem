4c45f255c08f15aad41965842c0659a18dfe9cee
Jason Kulatunga <jason@thesparktree.com>
2023-05-15 18:48:00 -0400

    adding tests to increase backend coverage.

---
+++ backend/pkg/auth/jwt_utils_test.go
@@ -0,0 +1,54 @@
+package auth_test
+
+import (
+	"github.com/fastenhealth/fastenhealth-onprem/backend/pkg/auth"
+	"github.com/fastenhealth/fastenhealth-onprem/backend/pkg/models"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+//TODO: this is broken
+func Test_JwtGenerateFastenTokenFromUser(t *testing.T) {
+	t.Parallel()
+
+	//setup
+	userData := models.User{
+		ModelBase: models.ModelBase{},
+		FullName:  "John Doe",
+		Username:  "john.doe@example.com",
+		Password:  "mysecretpassword",
+	}
+	encryptionKey := "thisismysupersecuressessionsecretlength"
+
+	//test
+	token, err := auth.JwtGenerateFastenTokenFromUser(userData, encryptionKey)
+
+	//require
+	require.NoError(t, err)
+	claims, validateErr := auth.JwtValidateFastenToken(encryptionKey, token)
+	require.NoError(t, validateErr)
+	require.Equal(t, "John Doe", claims.FullName)
+	require.Equal(t, "John Doe", claims.UserMetadata.FullName)
+	require.Equal(t, "john.doe@example.com", claims.Subject)
+	require.Equal(t, "john.doe@example.com", claims.Email)
+}
+
+//TODO: this is broken
+func Test_JwtGenerateFastenTokenFromUser_WithEmptyEncryptionKey(t *testing.T) {
+	t.Parallel()
+
+	//setup
+	userData := models.User{
+		ModelBase: models.ModelBase{},
+		FullName:  "John Doe",
+		Username:  "john.doe",
+		Password:  "mysecretpassword",
+	}
+	encryptionKey := ""
+
+	//test
+	_, err := auth.JwtGenerateFastenTokenFromUser(userData, encryptionKey)
+
+	//require
+	require.Error(t, err)
+}
---
+++ backend/pkg/errors/errors_test.go
@@ -0,0 +1,15 @@
+package errors_test
+
+import (
+	"github.com/fastenhealth/fastenhealth-onprem/backend/pkg/errors"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestErrors(t *testing.T) {
+	t.Parallel()
+
+	//assert
+	require.Implements(t, (*error)(nil), errors.ConfigFileMissingError("test"), "should implement the error interface")
+	require.Implements(t, (*error)(nil), errors.ConfigValidationError("test"), "should implement the error interface")
+}
