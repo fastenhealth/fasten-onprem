991732bf5c06b95076cdade872ae76f6225dd148
Jason Kulatunga <jason@thesparktree.com>
2023-05-21 19:45:35 -0700

    dynamically extract values from objects instead of from array of numbers.

--- frontend/src/app/widgets/simple-line-chart-widget/simple-line-chart-widget.component.ts
+++ frontend/src/app/widgets/simple-line-chart-widget/simple-line-chart-widget.component.ts
@@ -26,24 +26,34 @@ export class SimpleLineChartWidgetComponent extends DashboardWidgetComponent imp
       this.chartData = [{
         label: 'This week',
         // data: [27.2, 29.9, 29.6, 25.7, 25.9, 29.3, 31.1, 27.9, 28.4, 25.4, 23.2, 18.2, 14, 12.7, 11, 13.7, 9.7, 12.6, 10.9, 12.7, 13.8, 12.9, 13.8, 10.2, 5.8, 7.6, 8.8, 5.6, 5.6, 6.3, 4.2, 3.6, 5.4, 6.5, 8.1, 10.9, 7.6, 9.7, 10.9, 9.5, 5.4, 4.9, .7, 2.3, 5.5, 10, 10.6, 8.3, 8.4, 8.5, 5.8 ],
-        data: [27.2, 29.9, 29.6, 25.7, 25.9, 29.3, 31.1, 27.9, 28.4, 25.4 ],
-        // data: [
-        //   {id: 'a', data: 27.2},
-        //   {id: 'b', data: 29.9},
-        //   {id: 'c', data: 29.6},
-        //   {id: 'd', data: 25.7},
-        //   {id: 'e', data: 25.9},
-        //   {id: 'f', data: 29.3},
-        //   {id: 'g', data: 31.1},
-        //   {id: 'h', data: 27.9},
-        //   {id: 'i', data: 28.4},
-        //   {id: 'j', data: 25.4}],
-
+        // data: [27.2, 29.9, 29.6, 25.7, 25.9, 29.3, 31.1, 27.9, 28.4, 25.4 ],
+        data: [
+          {id: 'a', data: 27.2},
+          {id: 'b', data: 29.9},
+          {id: 'c', data: 29.6},
+          {id: 'd', data: 25.7},
+          {id: 'e', data: 25.9},
+          {id: 'f', data: 29.3},
+          {id: 'g', data: 31.1},
+          {id: 'h', data: 27.9},
+          {id: 'i', data: 28.4},
+          {id: 'j', data: 25.4},
+          {id: 'k', data: 27.2},
+          {id: 'l', data: 29.9},
+          {id: 'm', data: 29.6},
+          {id: 'n', data: 25.7},
+          {id: 'o', data: 25.9},
+          {id: 'p', data: 29.3},
+          {id: 'q', data: 31.1},
+          {id: 'r', data: 27.9},
+          {id: 's', data: 28.4},
+          {id: 't', data: 25.4}
+        ],
         // parsing: {
         //   xAxisKey: 'id',
         //   yAxisKey: 'data'
         // },
-      }] as ChartConfiguration<'line'>['data']['datasets']
+      }] as unknown as ChartConfiguration<'line'>['data']['datasets']
     // }
   }
 
