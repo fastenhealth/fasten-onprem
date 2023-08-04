cb78701f7f61ee1fcdcc14f1aa1f27395708d166
OhHeyAlan <ohheyalan@me.com>
2023-08-04 00:20:09 -0500

    Update list-encounter.component.ts

--- frontend/src/app/components/list-generic-resource/list-encounter.component.ts
+++ frontend/src/app/components/list-generic-resource/list-encounter.component.ts
@@ -8,9 +8,12 @@ import {GenericColumnDefn, ListGenericResourceComponent, ResourceListComponentIn
 })
 export class ListEncounterComponent extends ListGenericResourceComponent  {
   columnDefinitions: GenericColumnDefn[] = [
-    { title: 'Encounter', versions: '*', format: 'code', getter: e => e.type[0].coding[0] },
+    // { title: 'Encounter', versions: '*', format: 'code', getter: e => e.type[0].coding[0] },
+    // { title: 'Encounter', versions: '*', getter: e => e.reasonCode[0].text },
     { title: 'Period', versions: '*', format: 'period', getter: e => e.period },
-    { title: 'Diagnosis', versions: '*', getter: e => e.diagnosis?.map(d => d.condition?.reference).join()},
-    { title: 'Discharge Disposition', versions: '*', format: 'code', getter: e => e.hospitalization?.dischargeDisposition.coding[0] }
+    { title: 'Encounter', versions: '*', getter: e => e.type[0].text },
+    { title: 'Reason', versions: '*', getter: e => e.reasonCode[0].text },
+    { title: 'Practioner', versions: '*', getter: e => e.participant[0].individual.display }, 
+    { title: 'Discharge Disposition', versions: '*', format: 'code', getter: e => e.hospitalization?.dischargeDisposition.coding[0] },
   ]
 }
