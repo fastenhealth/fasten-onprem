54e644bfed12d02f346af4c25b6554f2b588b7ac
OhHeyAlan <ohheyalan@me.com>
2023-08-04 00:12:05 -0500

    Update list-service-request.component.ts

--- frontend/src/app/components/list-generic-resource/list-service-request.component.ts
+++ frontend/src/app/components/list-generic-resource/list-service-request.component.ts
@@ -9,12 +9,15 @@ import {attributeXTime} from './utils';
 })
 export class ListServiceRequestComponent extends ListGenericResourceComponent {
   columnDefinitions: GenericColumnDefn[] = [
-    { title: 'Service', versions: '*', format: 'code', getter: s => s.code.coding[0] },
+    // orig { title: 'Service', versions: '*', format: 'code', getter: s => s.code.coding[0] },
     { title: 'Author Date', versions: '*', format: 'date', getter: s => s.authoredOn },
+    { title: 'Service', versions: '*', getter: s => s.category[0].text },
+    { title: 'Ordered', versions: '*', getter: s => s.code.text },
+    { title: 'Dx', versions: '*', format: 'code', getter: s => s.reasonCode[0].coding[0] }, //Was Task
+    { title: 'Ordered By', versions: '*', getter: s => s.requester.display },
     { title: 'Status', versions: '*', getter: s => s.status },
-    { title: 'Reason', versions: '*', format: 'code', getter: s => s.reasonCode[0].coding[0] },
-    { title: 'ID', versions: '*', getter: s => s.id },
-    { title: 'Do Not Perform', versions: '*', getter: s => s.doNotPerform },
-    { title: 'Reason Refused', versions: '*', format: 'code', getter: s => s.extension.reasonRefused.coding[0] }
+    // Useless { title: 'ID', versions: '*', getter: s => s.id },
+    // Not used { title: 'Do Not Perform', versions: '*', getter: s => s.doNotPerform },
+    // Not used { title: 'Reason Refused', versions: '*', format: 'code', getter: s => s.extension.reasonRefused.coding[0] }
   ]
 }
