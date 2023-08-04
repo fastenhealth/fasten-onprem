8bd2e8eb04dd920d5fd67e43f044796597d97413
OhHeyAlan <ohheyalan@me.com>
2023-08-04 00:22:24 -0500

    Update list-allergy-intolerance.component.ts

--- frontend/src/app/components/list-generic-resource/list-allergy-intolerance.component.ts
+++ frontend/src/app/components/list-generic-resource/list-allergy-intolerance.component.ts
@@ -9,8 +9,11 @@ import {attributeXTime} from './utils';
 })
 export class ListAllergyIntoleranceComponent extends ListGenericResourceComponent {
   columnDefinitions: GenericColumnDefn[] = [
-    { title: 'Allergy', versions: '*', format: 'code', getter: a => a.code.coding[0] },
     { title: 'Date Recorded', versions: '*',  format: 'date', getter: a => a.assertedDate || a.recordedDate },
+    // orig { title: 'Allergy', versions: '*', format: 'code', getter: a => a.code.coding[0] },
+    { title: 'Allergy Type', versions: '*', getter: a => a.category[0] }, // Allergy Type
+    { title: 'Allergic To', versions: '*', getter: a => a.code.text }, // Substance
+    { title: 'Reaction', versions: '*', getter: a => a.reaction[0].manifestation[0].text }, // Reaction
     { title: 'Onset', versions: '*', format: 'date', getter: a => a.onsetDateTime },
     { title: 'Resolution Age', versions: '*', format: 'date', getter: a => a.extension.resolutionAge }
   ]
