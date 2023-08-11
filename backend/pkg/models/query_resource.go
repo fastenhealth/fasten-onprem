df8fffbadeb2021843b1a22bd10b4a0d401ceb1d
Jason Kulatunga <jason@thesparktree.com>
2023-08-11 14:24:11 -0600

    starting to work on GroupBy aggregations

--- backend/pkg/models/query_resource.go
+++ backend/pkg/models/query_resource.go
@@ -1,9 +1,22 @@
 package models
 
+type QueryResourceAggregationType string
+
+const (
+	QueryResourceAggregationTypeCountBy QueryResourceAggregationType = "countBy"
+	QueryResourceAggregationTypeGroupBy QueryResourceAggregationType = "groupBy"
+	QueryResourceAggregationTypeMinBy   QueryResourceAggregationType = "minBy"
+	QueryResourceAggregationTypeMaxBy   QueryResourceAggregationType = "maxBy"
+)
+
 // maps to frontend/src/app/models/widget/dashboard-widget-query.ts
 type QueryResource struct {
 	Use    string                 `json:"use"`
 	Select []string               `json:"select"`
 	From   string                 `json:"from"`
 	Where  map[string]interface{} `json:"where"`
+
+	//aggregation fields
+	AggregationParams []string                      `json:"aggregation_params"`
+	AggregationType   *QueryResourceAggregationType `json:"aggregation_type"`
 }
