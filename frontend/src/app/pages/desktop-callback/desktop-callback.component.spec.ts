cac69f76d8fe16adba92961ea0a8908029170449
Jason Kulatunga <jason@thesparktree.com>
2023-09-07 16:58:02 -0700

    fixing test.

--- frontend/src/app/pages/desktop-callback/desktop-callback.component.spec.ts
+++ frontend/src/app/pages/desktop-callback/desktop-callback.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DesktopCallbackComponent } from './desktop-callback.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 describe('DesktopCallbackComponent', () => {
   let component: DesktopCallbackComponent;
@@ -8,6 +11,7 @@ describe('DesktopCallbackComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
       declarations: [ DesktopCallbackComponent ]
     })
     .compileComponents();
