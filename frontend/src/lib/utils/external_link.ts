7dc03189314f09f6e3287ca6fdf7e47d81c4d597
Jason Kulatunga <jason@thesparktree.com>
2023-09-07 16:07:46 -0700

    adding loading spinner to desktop callback. make sure modal popup is closed in sources window when app is redirected.

--- frontend/src/lib/utils/external_link.ts
+++ frontend/src/lib/utils/external_link.ts
@@ -1,4 +1,4 @@
-export function OpenExternalLink(url: string, desktopMode: boolean){
+export function OpenExternalLink(url: string, desktopMode: boolean, windowId?: string){
   //check if url starts with https, and if not, prepend it (external links are never relative)
   if(!url.startsWith("https://") && !url.startsWith("http://")){
     url = "https://" + url;
@@ -6,7 +6,7 @@ export function OpenExternalLink(url: string, desktopMode: boolean){
 
   //check if wails exists and is defined
   if(typeof wails !== "undefined" && desktopMode){
-    wails.CallByName("pkg.AppService.BrowserOpenURL", url)
+    wails.CallByName("pkg.AppService.BrowserOpenURL", url, windowId || 'external')
   } else{
     window.open(url, "_blank");
   }
