cde7b154f788e1546c74bd80d27c7ba255b7a86e
Jason Kulatunga <jason@thesparktree.com>
2023-11-22 19:07:17 -0800

    trying to fix broken observations
    related #194

--- frontend/src/app/components/report-labs-observation/report-labs-observation.component.ts
+++ frontend/src/app/components/report-labs-observation/report-labs-observation.component.ts
@@ -144,6 +144,12 @@ export class ReportLabsObservationComponent implements OnInit {
       )
 
       //get current value
+      // let currentValue = fhirpath.evaluate(observation.resource_raw, "Observation.valueQuantity.value")[0]
+      // if(currentValue != null){
+      //   currentValues.push([currentValue, currentValue])
+      // } else {
+      //   currentValues.push([])
+      // }
       currentValues.push(fhirpath.evaluate(observation.resource_raw, "Observation.valueQuantity.value")[0])
 
       //set chart x-axis label
@@ -158,6 +164,13 @@ export class ReportLabsObservationComponent implements OnInit {
 
 
       //add low/high ref value blocks
+      // let referenceLow = fhirpath.evaluate(observation.resource_raw, "Observation.referenceRange.low.value")[0]
+      // let referenceHigh = fhirpath.evaluate(observation.resource_raw, "Observation.referenceRange.high.value")[0]
+      // if (referenceLow != null && referenceHigh != null){
+      //   referenceRanges.push([referenceLow, referenceHigh])
+      // } else {
+      //   referenceRanges.push([0,0])
+      // }
       referenceRanges.push([
         fhirpath.evaluate(observation.resource_raw, "Observation.referenceRange.low.value")[0],
         fhirpath.evaluate(observation.resource_raw, "Observation.referenceRange.high.value")[0]
@@ -169,6 +182,10 @@ export class ReportLabsObservationComponent implements OnInit {
     // @ts-ignore
     this.barChartData[0].data = referenceRanges
     this.barChartData[1].data = currentValues.map(v => [v, v])
+    // this.barChartData[1].data = currentValues
+
+    console.log(this.observationTitle, this.barChartData[0].data, this.barChartData[1].data)
+
 
     if(currentValues.length > 1){
       this.chartHeight = 30 * currentValues.length
