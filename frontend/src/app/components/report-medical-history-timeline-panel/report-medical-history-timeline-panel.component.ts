87ebeddc8f0352a5c13e9f702fcd076f5d1405ab
Jason Kulatunga <jason@thesparktree.com>
2023-11-22 16:51:00 -0800

    make sure diagnostic reports that are lab results link to the Lab page, otherwise show the Explore page data.

--- frontend/src/app/components/report-medical-history-timeline-panel/report-medical-history-timeline-panel.component.ts
+++ frontend/src/app/components/report-medical-history-timeline-panel/report-medical-history-timeline-panel.component.ts
@@ -5,6 +5,10 @@ import {RecResourceRelatedDisplayModel} from '../../../lib/utils/resource_relate
 import {LocationModel} from '../../../lib/models/resources/location-model';
 import {PractitionerModel} from '../../../lib/models/resources/practitioner-model';
 import {OrganizationModel} from '../../../lib/models/resources/organization-model';
+import {fhirModelFactory} from '../../../lib/models/factory';
+import {ResourceType} from '../../../lib/models/constants';
+import {DiagnosticReportModel} from '../../../lib/models/resources/diagnostic-report-model';
+import {FastenDisplayModel} from '../../../lib/models/fasten/fasten-display-model';
 
 @Component({
   selector: 'app-report-medical-history-timeline-panel',
@@ -21,6 +25,14 @@ export class ReportMedicalHistoryTimelinePanelComponent implements OnInit {
 
     let parsed = RecResourceRelatedDisplayModel(this.resourceFhir)
     this.displayModel = parsed.displayModel as EncounterModel
+  }
+
+  diagnosticReportLink(diagnosticReportRaw: FastenDisplayModel): string {
+    console.log('diagnosticReportRaw', diagnosticReportRaw)
+    let diagnosticReport = diagnosticReportRaw as DiagnosticReportModel
+    return diagnosticReport?.is_category_lab_report ?
+      '/labs/report/'+ diagnosticReport?.source_id + '/' + diagnosticReport?.source_resource_type + '/' + diagnosticReport?.source_resource_id :
+      '/explore/'+ diagnosticReport?.source_id + '/resource/' + diagnosticReport?.source_resource_id + '/'
   }
 
 }
