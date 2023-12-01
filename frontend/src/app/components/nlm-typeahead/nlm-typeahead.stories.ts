4ebe875505cc48cdb3ae9a5fbfdc219ba3002704
Jason Kulatunga <jason@thesparktree.com>
2023-11-30 19:06:48 -0800

    make sure nlm typeahead makes network requests.

--- frontend/src/app/components/nlm-typeahead/nlm-typeahead.stories.ts
+++ frontend/src/app/components/nlm-typeahead/nlm-typeahead.stories.ts
@@ -1,7 +1,7 @@
 import type {Meta, StoryObj} from '@storybook/angular';
 import {applicationConfig} from '@storybook/angular';
 import {NlmSearchType, NlmTypeaheadComponent} from './nlm-typeahead.component';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {HTTP_CLIENT_TOKEN} from '../../dependency-injection';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {importProvidersFrom} from '@angular/core';
@@ -16,7 +16,7 @@ const meta: Meta<NlmTypeaheadComponent> = {
     // })
     applicationConfig({
       providers: [
-        importProvidersFrom(HttpClientTestingModule),
+        importProvidersFrom(HttpClientModule),
         {
           provide: HttpClient,
           useClass: HttpClient
@@ -51,102 +51,119 @@ type Story = StoryObj<NlmTypeaheadComponent>;
 // More on writing stories with args: https://storybook.js.org/docs/angular/writing-stories/args
 export const Allergy: Story = {
   args: {
-    searchType: NlmSearchType.Allergy
+    searchType: NlmSearchType.Allergy,
+    debugMode: true
   }
 };
 
 export const AllergyReaction: Story = {
   args: {
-    searchType: NlmSearchType.AllergyReaction
+    searchType: NlmSearchType.AllergyReaction,
+    debugMode: true
   }
 };
 
 export const Condition: Story = {
   args: {
-    searchType: NlmSearchType.Condition
+    searchType: NlmSearchType.Condition,
+    debugMode: true
   }
 };
 
 export const MedicalContactIndividualProfession: Story = {
   args: {
-    searchType: NlmSearchType.MedicalContactIndividualProfession
+    searchType: NlmSearchType.MedicalContactIndividualProfession,
+    debugMode: true
   }
 };
 
 export const MedicalContactIndividual: Story = {
   args: {
-    searchType: NlmSearchType.MedicalContactIndividual
+    searchType: NlmSearchType.MedicalContactIndividual,
+    debugMode: true
   }
 };
 
 export const MedicalContactOrganization: Story = {
   args: {
-    searchType: NlmSearchType.MedicalContactOrganization
+    searchType: NlmSearchType.MedicalContactOrganization,
+    debugMode: true
   }
 };
 
 export const MedicalContactOrganizationType: Story = {
   args: {
-    searchType: NlmSearchType.MedicalContactOrganizationType
+    searchType: NlmSearchType.MedicalContactOrganizationType,
+    debugMode: true
   }
 };
 
 export const Medication: Story = {
   args: {
-    searchType: NlmSearchType.Medication
+    searchType: NlmSearchType.Medication,
+    debugMode: true
   }
 };
 
 export const MedicationWhyStopped: Story = {
   args: {
-    searchType: NlmSearchType.MedicationWhyStopped
+    searchType: NlmSearchType.MedicationWhyStopped,
+    debugMode: true
   }
 };
 
 export const Procedure: Story = {
   args: {
-    searchType: NlmSearchType.Procedure
+    searchType: NlmSearchType.Procedure,
+    debugMode: true
   }
 };
 
 export const Vaccine: Story = {
   args: {
-    searchType: NlmSearchType.Vaccine
+    searchType: NlmSearchType.Vaccine,
+    debugMode: true
   }
 };
 
 export const Countries: Story = {
   args: {
-    searchType: NlmSearchType.Countries
+    searchType: NlmSearchType.Countries,
+    debugMode: true
   }
 };
 
 export const AttachmentFileType: Story = {
   args: {
-    searchType: NlmSearchType.AttachmentFileType
+    searchType: NlmSearchType.AttachmentFileType,
+    debugMode: true
   }
 };
 
 export const AttachmentCategory: Story = {
   args: {
-    searchType: NlmSearchType.AttachmentCategory
+    searchType: NlmSearchType.AttachmentCategory,
+    debugMode: true
   }
 };
 
 export const EncounterClassification: Story = {
   args: {
-    searchType: NlmSearchType.EncounterClassification
+    searchType: NlmSearchType.EncounterClassification,
+    debugMode: true
   }
 };
 
 export const EncounterServiceType: Story = {
   args: {
-    searchType: NlmSearchType.EncounterServiceType
+    searchType: NlmSearchType.EncounterServiceType,
+    debugMode: true
   }
 };
 
 export const PrePopulated: Story = {
   args: {
-    searchType: NlmSearchType.PrePopulated
+    searchType: NlmSearchType.PrePopulated,
+    debugMode: true
   }
 };
