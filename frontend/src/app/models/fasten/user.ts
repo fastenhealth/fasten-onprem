db893f66e1d8ea7ffde3363c98a5b57626f8ce18
Jason Kulatunga <jason@thesparktree.com>
2024-02-14 17:15:12 -0800

    fixing styles. Error handling.

--- frontend/src/app/models/fasten/user.ts
+++ frontend/src/app/models/fasten/user.ts
@@ -2,5 +2,6 @@ export class User {
   user_id?: number
   full_name?: string
   username?: string
+  email?: string
   password?: string
 }
--- frontend/src/app/pages/auth-signup-wizard/auth-signup-wizard.component.ts
+++ frontend/src/app/pages/auth-signup-wizard/auth-signup-wizard.component.ts
@@ -1,5 +1,17 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {User} from '../../models/fasten/user';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
+import {ToastService} from '../../services/toast.service';
+import {ToastNotification, ToastType} from '../../models/fasten/toast';
+
+class UserWizard extends User {
+  passwordConfirm: string = ""
+  agreeTerms: boolean = false
+  joinMailingList: boolean = true
+}
+
 
 @Component({
   selector: 'app-auth-signup-wizard',
@@ -95,9 +107,48 @@ export class AuthSignupWizardComponent implements OnInit {
     "b53ff282-4725-45f7-a436-89f77320f062.png",
   ]
 
-  constructor() { }
+  loading: boolean = false
+
+  submitted: boolean = false
+  newUser: UserWizard = new UserWizard()
+  errorMsg: string = ""
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastService: ToastService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  signupSubmit(){
+    this.loading = true
+    this.submitted = true;
+
+    this.authService.Signup(this.newUser).then((tokenResp: any) => {
+        this.loading = false
+        console.log(tokenResp);
+        this.router.navigateByUrl('/dashboard');
+      },
+      (err)=>{
+        this.loading = false
+        console.error("an error occured while signup",err)
+        if(err.name === 'conflict') {
+          // "batman" already exists, choose another username
+          this.errorMsg = "username already exists"
+        } else if (err.name === 'forbidden') {
+          // invalid username
+          this.errorMsg = "invalid username"
+        } else {
+          this.errorMsg = "an unknown error occurred during sign-up"
+        }
+
+        const toastNotificaiton = new ToastNotification()
+        toastNotificaiton.type = ToastType.Error
+        toastNotificaiton.message = this.errorMsg
+        this.toastService.show(toastNotificaiton)
+      })
   }
 
 }
