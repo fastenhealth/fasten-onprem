0eb86001897c931eb6476029c40294bc17e1dc5e
Jean Fernandez <jean.fernandez.code@gmail.com>
2024-03-05 16:27:42 -0500

    Prevent possible endless loop with image fallback

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,6 +1,7 @@
 import type { StorybookConfig } from "@storybook/angular";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: [{ from: '../src/assets', to: '/assets' }],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
