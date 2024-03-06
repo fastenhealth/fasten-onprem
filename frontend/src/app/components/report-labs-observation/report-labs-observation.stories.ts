c487c9fe25f4c697ed62bac1eb63e2eb7c10890e
Jean Fernandez <55406257+jean-the-coder@users.noreply.github.com>
2024-03-06 13:54:15 -0500

    Fix horizontal overflow for glossary lookup (#445)
    * Fix Overflow for Labs Glossary
    
    * Add story to glossary lookup and report labs observation

---
+++ frontend/src/app/components/report-labs-observation/report-labs-observation.stories.ts
@@ -0,0 +1,101 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
+import { DecoratorFunction } from '@storybook/types';
+import { ReportLabsObservationComponent } from './report-labs-observation.component'
+import { PipesModule } from 'src/app/pipes/pipes.module';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
+import { ResourceFhir } from 'src/app/models/fasten/resource_fhir';
+import { GlossaryLookupComponent } from '../glossary-lookup/glossary-lookup.component';
+import { NgChartsModule } from 'ng2-charts';
+import { HTTP_CLIENT_TOKEN } from 'src/app/dependency-injection';
+import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable, of } from 'rxjs';
+
+import R4Example1Json from "../../../lib/fixtures/r4/resources/observation/example1.json";
+import { Html as GlossaryLookupHtml } from '../glossary-lookup/glossary-lookup.stories';
+
+
+const withHttpClientProvider: DecoratorFunction<any> = (storyFunc, context) => {
+  const { httpClientResp } = context.parameters;
+
+  class MockHttpClient extends HttpClient {
+
+    get(): Observable<any> {
+      return of(httpClientResp)
+    }
+  }
+
+  return moduleMetadata({ providers: [{ provide: HTTP_CLIENT_TOKEN, useClass: MockHttpClient }] })(
+    storyFunc,
+    context
+  );
+};
+
+// More on how to set up stories at: https://storybook.js.org/docs/angular/writing-stories/introduction
+const meta: Meta<ReportLabsObservationComponent> = {
+  title: 'Components/ReportLabsObservation',
+  component: ReportLabsObservationComponent,
+  decorators: [
+    withHttpClientProvider,
+    moduleMetadata({
+      imports: [PipesModule, GlossaryLookupComponent, NgChartsModule, RouterTestingModule, HttpClientModule],
+      declarations: [NgbCollapse],
+      providers: [],
+    })
+  ],
+  tags: ['autodocs'],
+  render: (args: ReportLabsObservationComponent) => ({
+    props: {
+      backgroundColor: null,
+      ...args,
+    },
+  }),
+  argTypes: {
+    observations: {
+      control: 'array'
+    },
+    observationCode: {
+      control: 'string'
+    },
+    observationTitle: {
+      control: 'string'
+    },
+  },
+};
+
+
+export default meta;
+type Story = StoryObj<ReportLabsObservationComponent>;
+
+const observation: ResourceFhir = {
+  source_id: '',
+  source_resource_id: '',
+  source_resource_type: 'Observation',
+  fhir_version: '4',
+  sort_title: 'sort',
+  sort_date: new Date(),
+  resource_raw: R4Example1Json,
+};
+
+const observation2: ResourceFhir = {
+  source_id: '',
+  source_resource_id: '',
+  source_resource_type: 'Observation',
+  fhir_version: '4',
+  sort_title: 'sort',
+  sort_date: new Date(),
+  resource_raw: R4Example1Json,
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/angular/writing-stories/args
+export const Entry: Story = {
+  args: {
+    observations: [observation, observation2],
+    observationCode: '788-0',
+    observationTitle: 'Erythrocyte distribution width [Ratio] by Automated count',
+  },
+  parameters: {
+    ...GlossaryLookupHtml.parameters
+  }
+};
