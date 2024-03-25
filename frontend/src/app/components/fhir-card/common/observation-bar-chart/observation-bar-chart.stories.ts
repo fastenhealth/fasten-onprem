a3005c3c38491c883475a9ff50a5b567c990c698
Jean Fernandez <jean.fernandez.code@gmail.com>
2024-03-24 23:19:23 -0400

    Update observation-bar-chart

--- frontend/src/app/components/fhir-card/common/observation-bar-chart/observation-bar-chart.stories.ts
+++ frontend/src/app/components/fhir-card/common/observation-bar-chart/observation-bar-chart.stories.ts
@@ -29,42 +29,54 @@ type Story = StoryObj<ObservationBarChartComponent>;
 
 export const NoRange: Story = {
   args: {
-    observations: [new ObservationModel(observationR4Factory.build(), fhirVersions.R4)]
+    observations: [new ObservationModel(observationR4Factory.valueQuantity().build(), fhirVersions.R4)]
   }
 };
 
-export const ValueStringWithRange: Story = {
+export const RangedValueQuantity: Story = {
+  args: {
+    observations: [new ObservationModel(observationR4Factory.valueQuantity({ comparator: '<' }).build(), fhirVersions.R4)]
+  }
+};
+
+export const RangedValueStringWithReferenceRange: Story = {
   args: {
     observations: [new ObservationModel(observationR4Factory.valueString('<10 IntlUnit/mL').referenceRangeOnlyHigh(50).build(), fhirVersions.R4)]
   }
 };
 
+export const ValueInteger: Story = {
+  args: {
+    observations: [new ObservationModel(observationR4Factory.valueInteger().build(), fhirVersions.R4)]
+  }
+};
+
 export const Range: Story = {
   args: {
-    observations: [new ObservationModel(observationR4Factory.referenceRange().build(), fhirVersions.R4)]
+    observations: [new ObservationModel(observationR4Factory.valueQuantity().referenceRange().build(), fhirVersions.R4)]
   }
 };
 
 export const RangeOnlyLow: Story = {
   args: {
-    observations: [new ObservationModel(observationR4Factory.referenceRangeOnlyLow().build(), fhirVersions.R4)]
+    observations: [new ObservationModel(observationR4Factory.valueQuantity().referenceRangeOnlyLow().build(), fhirVersions.R4)]
   }
 };
 
 export const RangeOnlyLowText: Story = {
   args: {
-    observations: [new ObservationModel(observationR4Factory.referenceRangeStringOnlyLow().build(), fhirVersions.R4)]
+    observations: [new ObservationModel(observationR4Factory.valueQuantity().referenceRangeStringOnlyLow().build(), fhirVersions.R4)]
   }
 };
 
 export const RangeOnlyHigh: Story = {
   args: {
-    observations: [new ObservationModel(observationR4Factory.referenceRangeOnlyHigh().build(), fhirVersions.R4)]
+    observations: [new ObservationModel(observationR4Factory.valueQuantity().referenceRangeOnlyHigh().build(), fhirVersions.R4)]
   }
 };
 
 export const RangeOnlyHighText: Story = {
   args: {
-    observations: [new ObservationModel(observationR4Factory.referenceRangeStringOnlyHigh().build(), fhirVersions.R4)]
+    observations: [new ObservationModel(observationR4Factory.valueQuantity().referenceRangeStringOnlyHigh().build(), fhirVersions.R4)]
   }
 };
