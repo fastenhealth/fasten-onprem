e5e2e0f87cc30f41d6a900e9509b7e87e3d5e07e
Jean Fernandez <jean.fernandez.code@gmail.com>
2024-04-23 11:30:27 -0400

    Fix broken image path

--- frontend/src/app/directives/image-fallback.directive.ts
+++ frontend/src/app/directives/image-fallback.directive.ts
@@ -1,6 +1,6 @@
 import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 
-const DEFAULT_IMAGE_FALLBACK_PATH: string = '/assets/images/no-image.svg';
+const DEFAULT_IMAGE_FALLBACK_PATH: string = 'assets/images/no-image.svg';
 
 @Directive({
   selector: 'img[imageFallback]'
