d08f7a6fb9e5512ad6a4eada8047a74b3ce4d848
Vadim @ TSA <hello@techstackapps.com>
2025-07-11 14:44:07 +0300

    chore: adjust startConversation method to return correct conversation object

--- frontend/src/app/services/typesense.service.ts
+++ frontend/src/app/services/typesense.service.ts
@@ -2,38 +2,47 @@ import { Injectable } from '@angular/core';
 import { Client } from 'typesense';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TypesenseService {
   private client: Client;
 
   constructor() {
     this.client = new Client({
-      nodes: [{
-        host: 'localhost',
-        port: 8108,
-        protocol: 'http'
-      }],
+      nodes: [
+        {
+          host: 'localhost',
+          port: 8108,
+          protocol: 'http',
+        },
+      ],
       apiKey: 'xyz123',
-      connectionTimeoutSeconds: 2
+      connectionTimeoutSeconds: 10,
     });
   }
 
-  async startConversation(query: string, collection: string, conversationModelId: string, queryBy: string, conversationId?: string): Promise<any> {
+  async startConversation(
+    query: string,
+    collection: string,
+    conversationModelId: string,
+    queryBy: string,
+    conversationId?: string
+  ): Promise<any> {
     const searchRequests = {
       searches: [
         {
           collection: collection,
           query_by: queryBy,
-          exclude_fields: 'embedding'
-        }
-      ]
+          include_fields: 'source_resource_type,sort_title,sort_date',
+          // exclude_fields: 'embedding'
+        },
+      ],
     };
 
     const commonParams: any = {
       q: query,
       conversation: true,
-      conversation_model_id: conversationModelId
+      conversation_model_id: conversationModelId,
     };
 
     if (conversationId) {
@@ -41,10 +50,13 @@ export class TypesenseService {
     }
 
     try {
-      const response: any = await this.client.multiSearch.perform(searchRequests, commonParams);
-      return response.results[0].conversation;
+      const response: any = await this.client.multiSearch.perform(
+        searchRequests,
+        commonParams
+      );
+      return response.conversation;
     } catch (error) {
-      console.error('Error starting conversation:', error);
+      console.error('Error starting conversation:', error.message);
       throw error;
     }
   }
