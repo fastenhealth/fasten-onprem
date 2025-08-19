3e6073fa2c40db5336b5e8fcc67d6a0e14e365d1
Theo@tsa.team <hello@techstackapps.com>
2025-08-19 15:47:43 +0300

    refactor: Refactor search init and index function on server run
    [RECORD-SEARCH]

--- backend/pkg/web/handler/search.go
+++ backend/pkg/web/handler/search.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/fastenhealth/fasten-onprem/backend/pkg"
 	"github.com/fastenhealth/fasten-onprem/backend/pkg/database"
-	search "github.com/fastenhealth/fasten-onprem/backend/pkg/search"
+	"github.com/fastenhealth/fasten-onprem/backend/pkg/search"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
