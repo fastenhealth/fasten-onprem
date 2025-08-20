8d1837181f5443dd22994a3fc991651eef22eabf
vadim@tsa.team <hello@techstackapps.com>
2025-08-20 15:34:31 +0300

    chore: remove unused constant

--- backend/pkg/constants.go
+++ backend/pkg/constants.go
@@ -24,7 +24,6 @@ const (
 
 	ContextKeyTypeAuthUsername    string = "AUTH_USERNAME"
 	ContextKeyTypeAuthToken       string = "AUTH_TOKEN"
-	ContextKeyTypeToken           string = "TOKEN"
 	ContextKeyTypeBackgroundJobID string = "BACKGROUND_JOB_ID"
 
 	FhirResourceTypeComposition string = "Composition"
