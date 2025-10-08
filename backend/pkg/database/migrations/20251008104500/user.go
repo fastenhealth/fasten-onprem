69323a3f8baec71e801c868caf359fdbd252acc6
Theo@tsa.team <hello@techstackapps.com>
2025-10-08 14:19:01 +0300

    feat: Add User migration to support new property, add generated password for compatibility on new users
    [AUTH-SUPPORT]

---
+++ backend/pkg/database/migrations/20251008104500/user.go
@@ -0,0 +1,25 @@
+package _20240813222836
+
+import (
+	"github.com/fastenhealth/fasten-onprem/backend/pkg/models"
+)
+
+type Role string
+
+const (
+	RoleUser  Role = "user"
+	RoleAdmin Role = "admin"
+)
+
+type User struct {
+	models.ModelBase
+	FullName string `json:"full_name"`
+	Username string `json:"username" gorm:"unique"`
+	Password string `json:"password"`
+	AuthType string `json:"auth_type"` //can be "local" or "oidc" types
+
+	//additional optional metadata that Fasten stores with users
+	Picture string `json:"picture"`
+	Email   string `json:"email"`
+	Role    Role   `json:"role"`
+}
