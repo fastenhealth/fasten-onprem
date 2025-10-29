5c230bc1e4a17d287d3f3e28ef1a22813e2068ab
Theo@tsa.team <hello@techstackapps.com>
2025-10-29 16:16:32 +0200

    feat: WIP on edit delegated record: add UI and integrate update endpoint
    [DELEGATED-ACCESS]

--- frontend/src/app/pages/resource-detail/resource-detail.component.ts
+++ frontend/src/app/pages/resource-detail/resource-detail.component.ts
@@ -5,7 +5,6 @@ import {ResourceFhir} from '../../models/fasten/resource_fhir';
 import {fhirModelFactory} from '../../../lib/models/factory';
 import {ResourceType} from '../../../lib/models/constants';
 import {FastenDisplayModel} from '../../../lib/models/fasten/fasten-display-model';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-resource-detail',
@@ -23,8 +22,12 @@ export class ResourceDetailComponent implements OnInit {
   resource: ResourceFhir = null
   displayModel: FastenDisplayModel = null
   isDelegatedResource: boolean = false;
+  
+  // Delegated resource edit
+  isJsonValid = true;
+  editableJson = '';
 
-  constructor(private fastenApi: FastenApiService, private router: Router, private route: ActivatedRoute, private modalService: NgbModal) {
+  constructor(private fastenApi: FastenApiService, private router: Router, private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
@@ -35,25 +38,7 @@ export class ResourceDetailComponent implements OnInit {
     this.loading = true
 
     if (this.isDelegatedResource && this.ownerUserId) {
-      this.fastenApi.getDelegatedResourceBySourceId(
-        this.ownerUserId,
-        this.route.snapshot.paramMap.get('source_id'),
-        this.route.snapshot.paramMap.get('resource_id')
-      ).subscribe((resourceFhir) => {
-        this.loading = false
-        this.resource = resourceFhir;
-        this.sourceId = this.route.snapshot.paramMap.get('source_id')
-        this.sourceName = "unknown" //TODO: populate this
-
-        try{
-          let parsed = fhirModelFactory(resourceFhir.source_resource_type as ResourceType, resourceFhir)
-          this.displayModel = parsed
-        } catch (e) {
-          console.error(e)
-        }
-      }, error => {
-        this.loading = false
-      });
+      this.getDelegatedResource();
       return;
     }
 
@@ -72,5 +57,59 @@ export class ResourceDetailComponent implements OnInit {
     }, error => {
       this.loading = false
     });
+  }
+
+  getDelegatedResource() {
+     this.fastenApi.getDelegatedResourceBySourceId(
+        this.ownerUserId,
+        this.route.snapshot.paramMap.get('source_id'),
+        this.route.snapshot.paramMap.get('resource_id')
+      ).subscribe((resourceFhir) => {
+        this.loading = false
+        this.resource = resourceFhir;
+        this.editableJson = JSON.stringify(this.resource.resource_raw, null, 2);
+        this.sourceId = this.route.snapshot.paramMap.get('source_id')
+        this.sourceName = "unknown" //TODO: populate this
+
+        try{
+          let parsed = fhirModelFactory(resourceFhir.source_resource_type as ResourceType, resourceFhir)
+          this.displayModel = parsed
+        } catch (e) {
+          console.error(e)
+        }
+      }, error => {
+        this.loading = false
+      });
+  }
+
+  validateJson(): boolean {
+    try {
+      JSON.parse(this.editableJson);
+      this.isJsonValid = true;
+    } catch (e) {
+      this.isJsonValid = false;
+    }
+    return this.isJsonValid;
+  }
+
+  saveEditedResource() {
+    const raw = this.editableJson;
+    try {
+      const parsed = JSON.parse(raw);
+      this.fastenApi.updateResource(
+        this.resource.source_resource_type,
+        this.resource.source_resource_id,
+        {
+          resource_raw: parsed
+        }
+      ).subscribe(() => {
+        alert('Resource updated successfully');
+        this.getDelegatedResource();
+      }, error => {
+        alert('Failed to update resource');
+      });
+    } catch {
+      alert('Invalid JSON');
+    }
   }
 }
