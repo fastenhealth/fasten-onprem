4033558f680344155f2611ec64a9ab1b6a396af8
Theo@tsa.team <hello@techstackapps.com>
2025-10-31 11:52:42 +0200

    fix: Adjust spec file on Resource Detail Component
    [DELEGATED-ACCESS]

--- frontend/src/app/pages/resource-detail/resource-detail.component.spec.ts
+++ frontend/src/app/pages/resource-detail/resource-detail.component.spec.ts
@@ -1,11 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResourceDetailComponent } from './resource-detail.component';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {RouterTestingModule} from '@angular/router/testing';
-import {ActivatedRoute, convertToParamMap} from '@angular/router';
-import {HTTP_CLIENT_TOKEN} from '../../dependency-injection';
-import {HttpClient} from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { HTTP_CLIENT_TOKEN } from '../../dependency-injection';
+import { HttpClient } from '@angular/common/http';
 import { LoadingSpinnerComponent } from 'src/app/components/loading-spinner/loading-spinner.component';
 import { of } from 'rxjs';
 
@@ -30,11 +30,11 @@ describe('ResourceDetailComponent', () => {
                 resource_id:
                   'b64.cmVzb3VyY2VfZmhpcjpiNjQuYzI5MWNtTmxPbUZsZEc1aE9qRXlNelExTmpjNE9UQXhNak0wTlRZM01ETT06UGF0aWVudDoxMjM0NTY3ODkwMTIzNDU2NzAz',
               }),
-              queryParams: of({
-                isDelegatedResource: 'true',
-                ownerUserId: 'user-1234',
-              }),
             },
+            queryParams: of({
+              isDelegatedResource: 'true',
+              ownerUserId: 'user-1234',
+            }),
           },
         },
         {
